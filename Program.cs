52b3825402e74bceb047d8c171b717c4926b54e1
samwizardry <samwizardry@gmail.com>
2024-11-05 22:30:52 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,35 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, services, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+});
+
+var app = builder.Build();
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate = "{Protocol} {RequestMethod} {RequestPath}{QueryString} responded {StatusCode} in {Elapsed:0.0000} ms";
+
+    options.EnrichDiagnosticContext = (IDiagnosticContext diagnosticContext, HttpContext httpContext) =>
+    {
+        diagnosticContext.Set("Protocol", httpContext.Request.Protocol);
+
+        if (httpContext.Request.QueryString.HasValue)
+        {
+            diagnosticContext.Set("QueryString", Uri.UnescapeDataString(httpContext.Request.QueryString.Value!));
+        }
+        else
+        {
+            diagnosticContext.Set("QueryString", "");
+        }
+    };
+});
+
+//app.UseRouting();
+
+app.MapGet("/", () => "Hello, World!");
+
+app.Run();
